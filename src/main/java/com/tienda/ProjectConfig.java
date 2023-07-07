0bb47ed7f432e0a45da1985e767f34ee98662c09
fab22 <you@example.com>
2023-07-06 23:43:19 -0600

    finalizacion semana 6

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -3,26 +3,26 @@ package com.tienda;
 
 import java.util.Locale;
 import org.springframework.context.MessageSource;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+
 @Configuration 
 public class ProjectConfig implements WebMvcConfigurer{
     
     @Bean
-    public SessionLocaleResolver localeResolver(){
+    public LocaleResolver localeResolver(){
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.getDefault());
         slr.setLocaleAttributeName("session.current.locale");
         slr.setTimeZoneAttributeName("session.current.timezone");
-        
+       
         return slr;
                 
     }
@@ -39,6 +39,7 @@ public class ProjectConfig implements WebMvcConfigurer{
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(localeChangeInterceptor());
     }
+    
     @Bean("messageSource")
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -8,4 +8,10 @@ import java.util.List;
 public interface CategoriaService {
     
     public List<Categoria> getCategorias (boolean activos);
+    
+    public Categoria getCategoria(Categoria categoria);
+    
+    public void save (Categoria categoria );
+    
+    public void delete(Categoria categoria);
 }
