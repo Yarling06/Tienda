1286f2d3ec5cca6589c953ae8ac494d8160e5e4a
fab22 <you@example.com>
2023-07-07 01:29:33 -0600

    finalizacion semana 8

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -3,6 +3,7 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -20,6 +21,10 @@ public class Categoria implements Serializable{
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name="id_categoria")
+    List<Producto> productos;
+            
     public Categoria() {
     }
 
