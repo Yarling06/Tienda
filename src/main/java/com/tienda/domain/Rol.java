7c8036919e2834ef7986e109755a67badffabcc0
fab22 <you@example.com>
2023-08-07 17:06:14 -0600

    finalizacion semana 10

---
+++ src/main/java/com/tienda/domain/Rol.java
@@ -0,0 +1,26 @@
+
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Data
+@Table (name = "rol" )
+public class Rol implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "ïd_rol")
+    private Long idRol;
+    
+    @NotEmpty
+    private String nombre;
+    
+    @Column(name = "id_usuariol")
+    private Long idUsuario;
+}
---
+++ src/main/java/com/tienda/service/UsuarioService.java
@@ -0,0 +1,11 @@
+
+package com.tienda.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+public interface UsuarioService {
+    
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
